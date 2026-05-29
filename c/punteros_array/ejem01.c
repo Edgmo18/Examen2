a197b5db15909d1e080d35b6049ae146557ab3d7
Edgmo18 <edgarfelipemorenoosorio@gmail.com>
2026-05-29 13:08:58 -0500

    Add files via upload

---
+++ c/punteros_array/ejem01.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	int datos[5] = {8, 3, 15, 6, 10};
+	
+	printf("Datos[0] = %d\n", datos[0]);
+	printf("Datos[1] = %d\n",datos[1]);
+	printf("Datos[2] = %d\n", datos[2]);
+	printf("Datos[3] = %d\n", datos[3]);
+	printf("Datos[4] = %d\n", datos[4]);
+	return 0;
+}
