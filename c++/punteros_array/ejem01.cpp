91a2e29b534083176d2131e3e4f462c478de512f
Edgmo18 <edgarfelipemorenoosorio@gmail.com>
2026-05-28 22:13:49 -0500

    Add files via upload

---
+++ c++/punteros_array/ejem01.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int datos[5] = {8, 3, 15, 6, 10};
+	
+	cout <<"Datos[0] = " << datos[0] << endl;
+	cout <<"Datos[1] = " << datos[1] << endl;
+	cout <<"Datos[2] = " << datos[2] << endl;
+	cout <<"Datos[3] = " << datos[3] << endl;
+	cout <<"Datos[4] = " << datos[4] << endl;
+	return 0;
+}
