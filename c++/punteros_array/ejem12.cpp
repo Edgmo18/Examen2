f23a7385fd8645828c58acb0d12a78b924edff36
Edgmo18 <edgarfelipemorenoosorio@gmail.com>
2026-05-28 22:31:49 -0500

    Add files via upload

---
+++ c++/punteros_array/ejem12.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	int m[2][3] = {
+		{1, 2, 3},
+		{4, 5, 6}
+    };
+    
+    int i;
+    int j;
+    
+    for(i=0; i<2; i++){
+    	for(j=0; j<3; j++){
+    		cout<< m[i][j] <<" ";
+		}
+		cout<<endl;
+	}
+    
+    return 0;
+}
